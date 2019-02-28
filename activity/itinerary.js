92e1bddb46ade02fa996836cb5a08d16b12fd3d3
Henry Yu <henry.yu1997@gmail.com>
2019-02-28 01:56:55 -0500

    more on initializeing database; start class design

---
+++ activity/itinerary.js
@@ -0,0 +1,40 @@
+
+class itinerary{
+    constructor(){
+        this.city = null
+        this.gender = null
+        this.age = null
+        this.traveller = null
+        this.fav_things = []
+        this.fav_rest_type = []
+        this.fav_rest_feat = []
+        this.fav_night = []
+    }
+
+    setCity(str){
+        this.city = str
+    }
+    setAge(int) {
+        this.age = int
+    }
+    setGender(str) {
+        this.gender = str
+    }
+    setTraveller(str) {
+        this.traveller = str
+    }
+    
+    setFavThings(arr) {
+        this.fav_things = arr
+    }
+    setFavRestType(arr) {
+        this.fav_rest_type = arr
+    }
+    setFavRestFeat(arr) {
+        this.fav_rest_feat = arr
+    } 
+    setFavNight(arr) {
+        this.fav_night = arr
+    }
+
+}
