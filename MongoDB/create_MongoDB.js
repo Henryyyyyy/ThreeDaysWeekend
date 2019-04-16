b30cf69995ff6f166299d7c667f633fca79fa9c4
Nicola Morris <nm169@duke.edu>
2019-04-16 16:31:47 -0400

    added more entires to database

--- MongoDB/create_MongoDB.js
+++ MongoDB/create_MongoDB.js
@@ -41,9 +41,40 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
     var dbo = db.db(DATABASE);
     var myobj = [
-        {name: "Patachou", district: "Downtown", meals: ["breakfast", "lunch"], budget: "$$", styles: ["laid back", "casual", "trendy", "foodie"], audience: ["couple", "family", "group", "solo"]},
-        { name: "Bluebeard", district: "Fountain Square", meals: ["dinner", "lunch"], budget: "$$", styles: ["trendy", "foodie", "popular bar"], 
-            features: ["great wine", "award-winning chef", "impressive cocktails", "craft beer"], audience: ["couple", "family", "group", "solo"] }
+        {name: "Tinker Street", district: "Herron Morton", meals: ["dinner"], budget: "$$", styles: ["trendy", "popular bar", "foodie"], 
+        features: ["great wine", "award-winning chef", "reservations", "craft beer"], audience: ["couple", "family", "group", "solo"]},
+        
+        {name: "The Eagle", district: "Mass Ave", meals: ["dinner", "lunch"], budget: "$", styles: ["trendy", "popular bar", "foodie"], 
+        features: ["patio seating", "craft beer"]},
+        
+        {name: "Bakersfield", district: "Mass Ave", meals: ["dinner", "lunch"], budget: "$", styles: ["trendy", "popular bar", "foodie"], 
+        features: ["patio seating", "craft beer"]},
+
+        {name: "Wildwood Market", district: "Fountain Square", meals: ["lunch"], budget: "$", styles: ["grab & go", "casual", "local favorite"], 
+        features: ["patio seating"]},
+
+        {name: "Union 50", district: "Mass Ave", meals: ["dinner"], budget: "$$", styles: ["trendy", "popular bar", "foodie"], 
+        features: ["great wine", "impressive cocktails", "reservations", "craft beer"]},
+
+        {name: "Livery", district: "Mass Ave", meals: ["dinner"], budget: "$$", styles: ["trendy", "popular bar", "foodie", "casual"], 
+        features: ["great wine", "impressive cocktails", "patio seating", "craft beer"]},
+
+        {name: "St. Elmos", district: "Downtown", meals: ["dinner"], budget: "$$$", styles: ["fine dining", "intimate"], 
+        features: ["great wine", "impressive cocktails", "reservations", "craft beer"]},
+
+        {name: "Vida", district: "Mass Ave", meals: ["dinner"], budget: "$$$", styles: ["fine dining", "popular bar", "initimate"], 
+        features: ["great wine", "impressive cocktails", "reservations", "craft beer"]},
+
+        {name: "Napolese", district: "Downtown", meals: ["dinner"], budget: "$$", styles: ["casual", "initimate"], 
+        features: ["great wine", "craft beer"]},
+
+        {name: "Subito", district: "Downtown", meals: ["lunch"], budget: "$", styles: ["casual", "grab & go"]},
+
+        {name: "Bru Burger", district: "Mass Ave", meals: ["dinner", "lunch"], budget: "$$", styles: ["trendy", "popular bar", "foodie", "casual"], 
+        features: ["impressive cocktails", "craft beer"]},
+
+        {name: "Ali'l Poke", district: "Downtown", meals: ["lunch"], budget: "$", styles: ["casual", "grab & go"]},
+        
     ];
     dbo.collection(COLLECTION).insertMany(myobj, function (err, res) {
         if (err) throw err;
@@ -52,7 +83,5 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
         db.close();
     });
 });
-
-
 
 
