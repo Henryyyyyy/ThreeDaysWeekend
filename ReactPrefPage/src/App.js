3575977c5494a156da6a975933a202730dfa0fc8
Nicola Morris <nm169@duke.edu>
2019-04-22 20:04:44 -0400

    fixed home page about section

--- ReactPrefPage/src/App.js
+++ ReactPrefPage/src/App.js
@@ -59,8 +59,46 @@ class App extends Component {
       </div>
     </div>
   </header>
+  <section id="about" className="about-section text-center">
+      <div classNamr="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">What We Do</h2>
+            <p className="text-white-50">A typical traveler spend more than four hours planning a weekend in a new city. Not anymore! We make sense of the travel review site chaos. 3DW generates carefully curated hour-by-hour itineraries with expert insights. The best hotels, restaurants, bars and things to do in one itinerary!
+              </p>
+              
+          </div>
+      
+      <div className="container">
+        <div className = "row">
+        <div className= "col-lg-4 mx-auto">
+          <h3 className="text-white mb-4"> Where to Stay</h3>
+          <img src="img/hotel.jpg" class="img-fluid" alt=""/>
 
-  {/* <section id="signup" className="signup-section">
+          <p className="text-white-50">The highest ranked hotels that work best for your trip. Hotels selected for both location and quality.</p>
+         
+        </div>
+  
+         <div className= "col-lg-4 mx-auto">
+          <h3 className="text-white mb-4"> Eat and Drink</h3>
+          <img src="img/food.jpeg" class="img-fluid" alt=""/>
+          <br></br>
+          <p className="text-white-50">Handpicked restaurants and bars just for you. The best restaurants optimized for your location.</p>
+        </div>
+  
+         <div className= "col-lg-4 mx-auto">
+          <h3 className="text-white mb-4"> Activities</h3>
+          <img src="img/activity.jpg" class="img-fluid" alt=""/>
+          <br></br>
+          <p className="text-white-50">Recommendations of the best things to do in a city provided by local experts</p>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+   <section id="signup" className="signup-section">
     <div className="container">
       <div className="row">
         <div className="col-md-10 col-lg-8 mx-auto text-center">
@@ -77,7 +115,7 @@ class App extends Component {
         </div>
       </div>
     </div>
-  </section> */}
+  </section> 
 
   <footer className="bg-black small text-center text-white-50">
     <div className="container">
