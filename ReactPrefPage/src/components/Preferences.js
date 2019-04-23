78495ecf4e5c6697ad44d39765391798f2711c02
Nicola Morris <nm169@duke.edu>
2019-04-23 01:07:50 -0400

    fixed shuffling

--- ReactPrefPage/src/components/Preferences.js
+++ ReactPrefPage/src/components/Preferences.js
@@ -30,32 +30,12 @@ class Preferences extends Component {
   
   componentWillMount() {
     const shuffledAnswerOptions = quizQuestions.map(question =>
-      this.shuffleArray(question.answers)
+      question.answers
     );
     this.setState({
       question: quizQuestions[0].question,
       answerOptions: shuffledAnswerOptions[0]
     });
-  }
-
-  shuffleArray(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
   }
 
   //setting the answer and then setting the next question
