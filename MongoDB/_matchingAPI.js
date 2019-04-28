e1b896015e8e13e55a4a2f2aff7d98587c3ad34b
HenryYu <aohan.yu@duke.edu>
2019-04-28 14:28:53 -0400

    matching ALG done. working on wrapping callback

--- MongoDB/_matchingAPI.js
+++ MongoDB/_matchingAPI.js
@@ -7,26 +7,15 @@ var COLLECTION = "Indianapolis"
 var URL = "mongodb://localhost:27017/";
 
 module.exports = {
-    InsertRestaurant,
     MatchRestaurants
-} 
-
-MatchRestaurants(["male", "12", "solo", "whatever", "trendy", "award-winning chef", "get fucking lit"])
-
-function InsertRestaurant(DbName, ColName, DocName){
-    MongoClient.connect(URL, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(DbName);
-        var myobj = { name: DocName };
-        dbo.collection(ColName).insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-      });
 }
 
-function MatchRestaurants(pref){
+let DEBUG = false;
+
+MatchRestaurants(["male", "12", "solo", "whatever", "trendy", "award-winning chef", "get fucking lit"], "breakfast")
+
+function MatchRestaurants(pref, meal){
+    RESULT = [""];
     MongoClient.connect(URL, function(err, db) {
         if (err) throw err;
         
@@ -38,23 +27,109 @@ function MatchRestaurants(pref){
         let audience = pref[2];
         let style = pref[4];
         let feature = pref[5];
+        var col = db.db("restaurants").collection(COLLECTION);
+        let matchedList = [] 
 
-        //search breakfast
-        var dbo = db.db("restaurants");
-        let matchedList = []
+        //query
+        function queryCollection(collection, query, callback){
+            collection.find(query).toArray(function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result.length >= 0) {
+                    matchedList.push(result);
+                    callback();
+                    db.close();
+                }
+            });
+        }
 
-        query_audience = {meals: "breakfast", audience: audience};
-        var res = dbo.collection(COLLECTION).find(query_audience).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-            return result;
+        //search
+        
+        let query = {meals: meal, audience: audience};
+        queryCollection(col, query, function(){
+            // if (DEBUG) console.log("audience ===========");
+            // if (DEBUG) console.log(matchedList);
         });
+        query = {meals: meal, styles: style};
+        queryCollection(col, query, function(){
+            // if (DEBUG) console.log("style ===========");
+            // if (DEBUG) console.log(matchedList);
+        });
+        query = {meals: meal, features: feature};
+        queryCollection(col, query, function(){
+            if (DEBUG) console.log("===========");
+            if (DEBUG) console.log(matchedList);
 
-        console.log(res);
-        
-        
-      });
-    
+            //now matchedList is complete. Rank!
+            let dic = countMatches(matchedList);
+            RESULT = rankMataches(dic);
+            // console.log(RESULT);
+            //no matches
+            // if (lst.length === 0): 
 
+            });
+    });
+    console.log(RESULT);
 }
+
+//given matchedList, count each obj id occurence
+function countMatches(ary){
+    var dict = {};
+    for (i = 0; i < ary.length; i++){
+        for (j = 0; j < ary[i].length; j++){
+            o = ary[i][j]["_id"];
+            if (hasOwnProperty(dict,o)) dict[o] = dict[o] + 1;
+            else dict[o] = 1;
+        }
+    }
+    if (DEBUG) console.log(dict);
+    return dict;
+}
+
+//return ary of three most frequent obj.
+function rankMataches(dic){
+    res = [];
+    if (isEmpty(dic)) return res;
+    for (i = 0; i < 3; i++){
+        max = 0;
+        obj = "";
+        //loop through dic obj
+        for (var key in dic) {
+            if (dic.hasOwnProperty(key)) {
+                if (dic[key] > max){
+                    obj = key;
+                    max = dic[key];
+                }
+            }
+        }
+        //end of loop
+        res.push(obj);
+        dic[obj] = -1;
+    }
+
+    //edge case: less than three matches
+    for (i = 0; i < res.length; i++){
+        if (res[i] === "") res[i] = res[0]; 
+    }
+
+    return res;
+}
+
+//check if an object has a property - helper for rankMataches
+function hasOwnProperty(obj, prop) {
+    var proto = obj.__proto__ || obj.constructor.prototype;
+    return (prop in obj) &&
+        (!(prop in proto) || proto[prop] !== obj[prop]);
+}
+
+//check if an obj is empty
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+
+    return true;
+}
+
+ 
